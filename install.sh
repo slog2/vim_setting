1c8b83c8350c6a147a25fe9ad1612950b1db4850
slog2 <slog24@nate.com>
2016-07-26 16:15:07 +0900

    Revert "test"
    This reverts commit f2dd7bb8002a2ed5029246ce933b56e6f9e72f76.

--- install.sh
+++ install.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
 
-#########################################
-# install.sh
-#
-# You shuold install git first  >  yum -y install git
-##########################################
+yum -y install git vim-enhanced
 
-mv ~/.vimrc ~/.vimrc.bak
-
-cp .vimrc ~/.vimrc
-cp -r .vim ~/ 
-
-yum -y install git git-core vim-enhanced
-
-mkdir ~/.vim/bundle/ -p
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
 
 vim +PluginInstall +qall
