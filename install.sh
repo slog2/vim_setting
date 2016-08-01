0ac00a442baf9b315a70c6a03531a5cbe734423d
slog2 <slog24@nate.com>
2016-08-01 16:24:06 +0900

    fixed

--- install.sh
+++ install.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
-
 #########################################
 # install.sh
 #
 # You shuold install git first  >  yum -y install git
 ##########################################
+cd $(dirname $0)
 
-mv ~/.vimrc ~/.vimrc.bak
+cp ~/.vimrc ~/.vimrc.bak
 
-cp .vimrc ~/.vimrc
+# copy .vimrc and .vim directory
+cp .vimrc ~/
 cp -r .vim ~/ 
 
+# install vim 
 yum -y install git git-core vim-enhanced
 
+# ignore Git SSL certificate problem 
+git config --global http.sslVerify false
+
+# install vim vundle plugin 
 mkdir ~/.vim/bundle/ -p
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
 
+# install all the plugins specified in .vimrc by vundle 
 vim +PluginInstall +qall
